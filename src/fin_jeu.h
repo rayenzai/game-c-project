6d31e16a299b6e84e0fe82df5e13f1dccff3a1f7
Lou <louansart4@gmail.com>
2026-02-23 17:42:51 +0100

    interaction avec la porte

---
+++ src/fin_jeu.h
@@ -0,0 +1,9 @@
+#ifndef FIN_JEU_H
+#define FIN_JEU_H
+
+#include <SDL2/SDL.h>
+
+//fin par rapport au choix
+void lancer_scene_fin(SDL_Renderer *renderer, int id_choix);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "intro.h"
 #include "menu.h"
 #include "game.h"
 #include "sons.h"
+#include "fin_jeu.h"
 
 #define WINDOW_WIDTH 960
 #define WINDOW_HEIGHT 720
